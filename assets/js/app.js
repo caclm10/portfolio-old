8503988871f9e88fbba49c1e68c7d0d3dfafec53
caclm10 <lewinxander@gmail.com>
2022-08-20 12:56:35 +0700

    add: responsive (incomplete)

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,17 +8,34 @@ function toggleActiveNavLink() {
     }
 }
 
-function centerNavbarBrand() {
-    // const homeLeft = document.querySelector('#home .content .left')
-    // const navbarBrand = document.querySelector(`.navbar-brand`)
-
-    // navbarBrand.style.width = `${homeLeft.clientWidth - 24}px`
+const media = {
+    smDown: window.matchMedia('(max-width: 575.98px)'),
+    smMd: window.matchMedia('(min-width: 576px) and (max-width: 767.98px)'),
+    mdUp: window.matchMedia('(min-width: 768px)')
 }
+
+function handleSmDown(p = {}) {
+    if (this.matches) {
+        p.swiper.params.slidesPerView = 1
+    }
+}
+
+function handleSmMd(p = {}) {
+    if (this.matches) {
+        p.swiper.params.slidesPerView = 2
+    }
+}
+
+function handleMdUp(p = {}) {
+    if (this.matches) {
+        p.swiper.params.slidesPerView = 3
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
     toggleActiveNavLink()
-    centerNavbarBrand()
 
     new fullScroll({
         mainElement: "main",
@@ -26,13 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
         dotsPosition: "left",
         animateTime: 0.7,
         animateFunction: "ease",
-    })
-
-    AOS.init()
-
-    const swiper = new Swiper(".project-slideshow", {
-        effect: "cards",
-        grabCursor: true,
     })
 
     const contactForm = document.querySelector(`form#contact-form`)
@@ -45,24 +55,36 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('hashchange', event => {
         toggleActiveNavLink()
 
-        const hash = window.location.hash
-        const sections = document.querySelectorAll(`section`)
+        // const hash = window.location.hash
+        // const sections = document.querySelectorAll(`section`)
 
-        for (const section of sections) {
-            if ('#' + section.dataset.index === hash) {
-                const aosElements = section.querySelectorAll(`[data-aos]`)
-                for (const aosElement of aosElements) {
-                    const delay = aosElement.dataset.aosDelay || '0'
+        // for (const section of sections) {
+        //     if ('#' + section.dataset.index === hash) {
+        //         const aosElements = section.querySelectorAll(`[data-aos]`)
+        //         for (const aosElement of aosElements) {
+        //             const delay = aosElement.dataset.aosDelay || '0'
 
-                    setTimeout(() => {
-                        aosElement.classList.add('aos-animate')
-                    }, +delay);
-                }
-            }
-        }
+        //             setTimeout(() => {
+        //                 aosElement.classList.add('aos-animate')
+        //             }, +delay);
+        //         }
+        //     }
+        // }
     })
 
-    window.addEventListener('resize', event => {
-        centerNavbarBrand()
-    })
+    var swiper = new Swiper(".swiper.content", {
+        slidesPerView: media.smDown.matches ? 1 : (media.smMd.matches ? 2 : 3),
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+
+
+
+    media.smDown.addEventListener('change', handleSmDown.bind(media.smDown, { swiper }))
+    media.smMd.addEventListener('change', handleSmMd.bind(media.smMd, { swiper }))
+    media.mdUp.addEventListener('change', handleMdUp.bind(media.mdUp, { swiper }))
 })
